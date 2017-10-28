1fbcbebc2a92d57d32b22c48c7f7ff3984f10513
Antonios Eleftheriadis <anthonyeleftheriades@gmail.com>
2017-10-28 12:45:10 +0300

    Add files via upload

---
+++ central_heating_system.ino
@@ -0,0 +1,19 @@
+
+
+
+int powerPin = 3;      // LED and relay connected to digital pin 9
+
+
+
+void setup() {
+  
+pinMode(powerPin, OUTPUT);   // sets the pin as output
+analogWrite(powerPin, 200);
+
+}
+
+void loop() {
+  
+  // put your main code here, to run repeatedly:
+
+}
